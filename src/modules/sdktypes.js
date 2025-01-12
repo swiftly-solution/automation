eda183cb93ec8fb039e27f1988cf85797092f61b
skuzzis <andreieurt@gmail.com>
2025-01-12 13:14:27 +0000

    update(types): Remove types with :

--- src/modules/sdktypes.js
+++ src/modules/sdktypes.js
@@ -2,13 +2,14 @@ export function ProcessSDKTypes(sdk) {
     var documentation = {}
     var output = {}
 
-    for (var enumName of Object.keys(sdk.types)) {
-        documentation[enumName.replace(/:/g, "_").toLowerCase()] = {
-            title: enumName.replace(/:/g, "_"),
+    for (const enumName of Object.keys(sdk.types)) {
+        if (enumName.includes(":")) continue
+        documentation[enumName.toLowerCase()] = {
+            title: enumName,
             template: "types-syntax",
             values: sdk.types[enumName].data
         }
-        output[enumName.replace(/:/g, "_")] = sdk.types[enumName].data
+        output[enumName] = sdk.types[enumName].data
     }
 
     return {
